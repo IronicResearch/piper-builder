a0194947ff0b68dba1d766e2bb4ead31a93ebc0f
Dave Milici <milici.dave@gmail.com>
2018-07-25 10:02:08 -0700

    Add builder for mpv/ffmpeg libs.

---
+++ mpv/build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "building mpv libs..."
+
+if [ ! -d ffmpeg ]; then
+	git clone https://github.com/FFmpeg/FFmpeg.git -b release/4.0
+fi
+
+echo "building ffmpeg libs..."
+
+pushd ffmpeg
+./configure --prefix=$EROOTFS/usr --enable-cross-compile --cross-prefix=$CROSS_COMPILE --sysroot=$ERPOTFS --arch=arm --target-os=linux --cpu=cortex-a53 --enable-vfp --enable-neon --enable-libdrm --enable-shared
+make -j4
+make install
+popd
+
+if [ ! -d mpv ]; then
+	git clone https://github.com/mpv-player/mpv.git -b release/0.29
+	pushd mpv
+	./bootstrap.py
+	popd
+fi
+
+echo "building mpv..."
+
+pushd mpv
+./waf configure --prefix=$EROOTFS/usr --enable-libmpv-shared --enable-drm --enable-openal --disable-libass --disable-wayland
+./waf build -j4
+./waf install
+popd
+
+exit $?
+
