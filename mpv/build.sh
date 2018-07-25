e8e8b840992454f3846a869ce4fff763f0c07585
Dave Milici <milici.dave@gmail.com>
2018-07-25 16:51:53 -0700

    Reduce ffmpeg codec support for Piper media formats.
    Includes MP4/H264/AAC, MP3, OGG/Vorbis, WAV/PCM.
    Note LZO, MOV, AIFF needed to complete compilation of mpv 0.29.

--- mpv/build.sh
+++ mpv/build.sh
@@ -9,7 +9,17 @@ fi
 echo "building ffmpeg libs..."
 
 pushd ffmpeg
-./configure --prefix=$EROOTFS/usr --enable-cross-compile --cross-prefix=$CROSS_COMPILE --sysroot=$EROOTFS --arch=arm --target-os=linux --cpu=cortex-a53 --enable-vfp --enable-neon --enable-libdrm --enable-shared
+./configure --prefix=$EROOTFS/usr --enable-cross-compile --cross-prefix=$CROSS_COMPILE --sysroot=$EROOTFS \
+	--arch=arm --target-os=linux --cpu=cortex-a53 --enable-vfp --enable-neon \
+	--enable-libdrm --enable-shared \
+	--disable-programs --disable-doc \
+	--disable-encoders --disable-muxers --disable-indevs \
+	--disable-filters --disable-protocols --disable-bsfs \
+	--disable-parsers --disable-demuxers --disable-decoders \
+	--enable-parser=aac,h264,mpeg4video,mpegaudio,vorbis \
+	--enable-demuxer=aac,h264,mpeg4,mp3,ogg,wav,pcm_s16le,mov,aiff \
+	--enable-decoder=aac,h264,mpeg4,mp3,vorbis,pcm_s16le \
+	--enable-lzo
 make -j4
 make install
 popd
