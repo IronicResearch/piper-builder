31c250180fdefd672b11f80ed9a4386616b2cfbb
Dave Milici <milici.dave@gmail.com>
2018-07-31 17:03:54 -0700

    Use builder relative path to irrlicht. (no tricky symlink)

--- minetest/build.sh
+++ minetest/build.sh
@@ -11,9 +11,9 @@ cmake . -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake \
 	-DRUN_IN_PLACE=1 -DBUILD_SERVER=0 \
 	-DENABLE_LUAJIT=1 \
 	-DLUA_INCLUDE_DIR=$EROOTFS/usr/include/luajit-2.0 \
-	-DIRRLICHT_SOURCE_DIR=irrlicht/source/Irrlicht \
-	-DIRRLICHT_INCLUDE_DIR=irrlicht/include \
-	-DIRRLICHT_LIBRARY=irrlicht/lib/Linux/libIrrlicht.a \
+	-DIRRLICHT_SOURCE_DIR=../../irrlicht/irrlicht/source/Irrlicht \
+	-DIRRLICHT_INCLUDE_DIR=../../irrlicht/irrlicht/include \
+	-DIRRLICHT_LIBRARY=../../irrlicht/irrlicht/lib/Linux/libIrrlicht.a \
 	-DENABLE_GLES=0
 make -j4
 popd
